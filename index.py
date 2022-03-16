8fa6cd5ca045307fea601c0fe3b0a061aee28b80
moghrabiabdallah <abdallah_moghrabi@outlook.com>
2022-03-16 12:39:21 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,18 @@
+# importing pandas package
+import seaborn as sns
+
+import pandas as pd
+sns.set_theme()
+ 
+# making data frame from csv file
+data = pd.read_csv("time_series_covid19_confirmed_global.csv", index_col="Country/Region"  )
+
+
+# retrieving row by loc method
+first = data.loc["France"]
+second = data.loc["Germany"]
+  
+  
+print(first, "\n\n\n" )
+sns.scatterplot( y='Country/Region' ,x='3/8/22',data=first)
+    
