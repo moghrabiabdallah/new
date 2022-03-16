656d7d729b36adf057cee5705eeb8e9bb495bc61
moghrabiabdallah <abdallah_moghrabi@outlook.com>
2022-03-16 14:20:09 +0300

    first commit

--- index.py
+++ index.py
@@ -7,11 +7,9 @@ sns.set_theme()
 # making data frame from csv file
 data = pd.read_csv("time_series_covid19_confirmed_global.csv", index_col="Country/Region"  )
 
-
 # retrieving row by loc method
 first = data.loc["France"]
 second = data.loc["Germany"]
-  
 print(first, "\n\n\n" )
 sns.scatterplot( y='Country/Region' ,x='3/8/22',data=first)
     
