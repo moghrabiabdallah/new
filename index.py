a7374fd6433701526fc61bb14cfd22df46539111
moghrabiabdallah <abdallah_moghrabi@outlook.com>
2022-03-16 12:52:43 +0300

    Initial commit

--- index.py
+++ index.py
@@ -12,7 +12,6 @@ data = pd.read_csv("time_series_covid19_confirmed_global.csv", index_col="Countr
 first = data.loc["France"]
 second = data.loc["Germany"]
   
-  
 print(first, "\n\n\n" )
 sns.scatterplot( y='Country/Region' ,x='3/8/22',data=first)
     
